71a188b4112f1a65e50dcbe1214633f1f59fceec
oguindon1 <91165057+oguindon@users.noreply.github.com>
2023-06-28 21:10:12 -0400

    Testing constructor test failed
    Testing that two boards equal created using the constructor using the same values are equal

--- lab04/tic_java/TicTest.java
+++ lab04/tic_java/TicTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 class TicTest {
 
 	@Test
-	void test() {
+	void testConstructor() {
 		Tic board = new Tic(3, 3);
 		Tic board2 = new Tic(3,3);
 		assertEquals(board, board2);
